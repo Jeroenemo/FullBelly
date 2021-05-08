81db49ea56b05e5d219f141b7b6d7f872916a50e
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-05-07 18:24:47 -0700

    update startup

--- Models/FullBellyContext.cs
+++ Models/FullBellyContext.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FullBelly.Models
 {
-    public class FullBellyAPIContext : IdentityDbContext
+    public class FullBellyContext : IdentityDbContext
     {
-        public FullBellyAPIContext(DbContextOptions<FullBellyAPIContext> options) : base(options) { }
+        public FullBellyContext(DbContextOptions<FullBellyContext> options) : base(options) { }
         public DbSet<Donation> Donations { get; set; }
         public DbSet<User> User { get; set; }
     }
