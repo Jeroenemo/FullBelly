7b004340f61216d8eec5a0890d06a6ee7f39fe78
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-05-13 20:44:09 -0700

    add header

---
+++ ClientApp/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Header';
+import { Jumbotron } from 'react-bootstrap';
+import './../styles/App.css'
+
+
+
+export default function App() {
+  
+
+
+  return (
+    <>
+    <Header />
+    <Jumbotron className="jumbotron">
+      
+      Hello World
+
+    </Jumbotron>
+    </>
+  );
+}
+
---
+++ ClientApp/src/components/Header.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Logo from './../img/header.png';
+import { Nav, Navbar } from 'react-bootstrap';
+import './../styles/Header.css';
+
+export default function Header() {
+
+  return (
+    <>
+    <Navbar className="nav-bar">
+      <Nav fill>
+        <img 
+        alt=""
+        src={Logo}
+        width="25%"
+        />
+        <Nav.Item className="nav-item">
+          <Nav.Link href="#home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#Donations">Make a Donation</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#Manage">Manage Donations</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Navbar>
+    </>
+  )
+}
