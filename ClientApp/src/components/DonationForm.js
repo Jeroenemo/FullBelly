7cd3ce827c251a2e2e5f78aaeee820ff75c8905d
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-05-16 10:53:35 -0700

    dink around with form

--- ClientApp/src/components/DonationForm.js
+++ ClientApp/src/components/DonationForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button, Card, Col , Form } from 'react-bootstrap';
 
 export default function DonationForm({ addDonation }) {
   const [value, setValue] = useState("");
@@ -12,6 +13,7 @@ export default function DonationForm({ addDonation }) {
 
   return (
     <>
+    <Card className="form-card">
       <form onSubmit={handleFormSubmission}>
         <input 
           type="text"
@@ -19,6 +21,58 @@ export default function DonationForm({ addDonation }) {
           value={value}
           onChange={event => setValue(event.target.value)}/>
       </form>
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Group controlId="formGridAddress1">
+          <Form.Label>Address</Form.Label>
+          <Form.Control placeholder="1234 Main St" />
+        </Form.Group>
+
+        <Form.Group controlId="formGridAddress2">
+          <Form.Label>Address 2</Form.Label>
+          <Form.Control placeholder="Apartment, studio, or floor" />
+        </Form.Group>
+
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>State</Form.Label>
+            <Form.Control as="select" defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>...</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridZip">
+            <Form.Label>Zip</Form.Label>
+            <Form.Control />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Group id="formGridCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Card>
     </>
   );
 }
