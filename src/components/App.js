1697fc40ecfccb4c741ccc60c774f02d0bbd3249
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-05-18 16:47:00 -0700

    remove google auth

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import DonationControl from './DonationControl';
 import Footer from './Footer';
 import Header from './Header';
-import Login from './Login';
-import Logout from './Logout';
 import { Jumbotron } from 'react-bootstrap';
 import './../styles/App.css';
 
@@ -11,8 +9,6 @@ export default function App() {
 
   return (
     <>
-    <Login />
-    <Logout />
     <Header />
     <Jumbotron>
       <DonationControl />
