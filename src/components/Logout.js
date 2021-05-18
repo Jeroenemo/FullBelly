2d8b74910e8d880bd29b12b1a1b538d99b3a52d6
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-05-17 20:51:21 -0700

    create logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -5,4 +5,14 @@ const clientId = `${process.env.YOUR_CLIENT_ID}`;
 
 export default function Logout() {
   const onSuccess = () => 
-}
+    alert('Logout made successfully');
+};
+
+return (
+  <>
+    <GoogleLogout
+      clientId={clientId}
+      buttonText="Lougout"
+      onLogoutSuccess={onSuccess} />
+  </>
+)
