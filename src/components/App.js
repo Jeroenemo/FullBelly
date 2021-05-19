4394de15440536ee7a1d8172f898909a94f88250
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-05-18 22:15:09 -0700

    move auth button to nav

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,6 @@ export default function App() {
     <Jumbotron>
       {loading}
     </Jumbotron>
-    <AuthenticationButton />
     <Footer />
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
 import { Context } from './../context/fullbellyContext';
 import Logo from './../img/header.png';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import AuthenticationButton from './AuthenticationButton';
 import './../styles/Header.css';
 
 export default function Header() {
@@ -18,12 +19,23 @@ export default function Header() {
         <Nav.Item>
           <Nav.Link onClick={() => setView('home')}>Home</Nav.Link>
         </Nav.Item>
+
         <Nav.Item>
           <Nav.Link onClick={() => setView('find')}>Find Donations</Nav.Link>
         </Nav.Item>
+
         <Nav.Item>
           <Nav.Link onClick={() => setView('form')}>Make a Donation</Nav.Link>
         </Nav.Item>
+
+        
+          <NavDropdown title="Manage">
+            <NavDropdown.Item>Manage Donations</NavDropdown.Item>
+            <NavDropdown.Item>
+              <AuthenticationButton />
+            </NavDropdown.Item>
+          </NavDropdown>
+        
       </Nav>
     </Navbar>
     </>
